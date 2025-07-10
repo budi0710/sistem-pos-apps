fee57434d5e60b1722cf95320b170306669010dd
lamhotsimamora <lamhotsimamora36@gmail.com>
2025-07-10 20:27:04 +0700

    +

--- app/Http/Controllers/AbsenController.php
+++ app/Http/Controllers/AbsenController.php
@@ -8,24 +8,30 @@ use DateTime;
 class AbsenController extends Controller
 {
     public function uploadAbsenMasuk(Request $request){
-       $file = $request->file('imageAbsenMasuk');
+        $file = $request->file('imageAbsenMasuk');
         $path = $file->store('absen', 'public');
 
         $absen = new Absens();
         $now = new DateTime();
+        // $now =  now();
 
-        $absen->id_user   = $request->session()->get('user_id');
-        $absen->date     = $now;
-        $absen->time_in  = date('H:i:s');
-        $absen->time_out = '';
-        $absen->image    = $path;
+        $absen->id_user = $request->session()->get('user_id');
+        $absen->date = date('Y-m-d');
+        $absen->time_in = date('H:i:s');
+        $absen->time_out = '00:00:00';
+        $absen->image = $path;
 
-        // check jika absen masuk dua kali 
-        $exist = Absens::whereDate('date', $now)->exists();
-        if ($exist){
-            return response()->json(['result'=>false]);
-        }else{
-            return $absen->save() ? response()->json(['result'=>true]) : response()->json(['result'=>false]);
+        // check jika absen masuk dua kali
+        // $exist = Absens::whereDate('date', $now)->exists();
+        $exist = Absens::where('date', date('Y-m-d'))
+            ->where('time_in', '<>', '00:00:00')
+            ->where('time_out', '=', '00:00:00')
+            ->count();
+
+        if ($exist) {
+            return response()->json(['result' => false]);
+        } else {
+            return $absen->save() ? response()->json(['result' => true]) : response()->json(['result' => false]);
         }
         
     }
