b75a2a6bad9eb71d356cd8381fe65fa685c43f4e
budi0710 <budi.ariyadi140886@gmail.com>
2025-08-07 07:27:12 +0700

    +

--- app/Http/Middleware/KeuanganMiddleware.php
+++ app/Http/Middleware/KeuanganMiddleware.php
@@ -10,12 +10,10 @@ class KeuanganMiddleware
     public function handle(Request $request, Closure $next)
     {
         $role = $request->session()->get('user_role');
-
         // Izinkan jika role adalah 1 atau 2
         if (in_array($role, [1, 2])) {
             return $next($request);
         }
-
         return redirect('404page');
     }
 }
